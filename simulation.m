c2a8ba99c2983f572ab7ced476fedb6ba8fec23e
edmundh <edmund.hunt@bristol.ac.uk>
2022-08-02 15:33:41 +0100

    Add files via upload
    Added grid based starting positions for agents, Jaccard similarity between target and estimated distribution using black & white images of plots

--- simulation.m
+++ simulation.m
@@ -35,16 +35,37 @@ region_info(2,:)=[60,30,10,0.5];
 region_info(3,:)=[80,80,15,0.8]; 
 [region3_x,region3_y]=circle_coords(region_info(3,1),region_info(3,2),region_info(3,3));
 
-%set agent positions
+%set agent positions. Assume agents are small enough to have 2 in same location
 
-for a=1:N_agents
+% for a=1:N_agents
+% %     agent_positions(a,1)=round(rand()*width_arena);   %for random start
+% %     agent_positions(a,2)=round(rand()*width_arena);
+% %     agent_position_history(1,1,a)=agent_positions(a,1);
+% %     agent_position_history(1,2,a)=agent_positions(a,2);
+% end
 
-    agent_positions(a,1)=round(rand()*width_arena);  %assume agents are small enough to have 2 in same location
-    agent_positions(a,2)=round(rand()*width_arena);
+grid_spacing=1; %set spacing in grid
+x=width_arena/2; %bottom left placement of first agent in grid
+y=width_arena/2; 
+grid_width=floor(sqrt(N_agents)); %the width of the grid
 
-    agent_position_history(1,1,a)=agent_positions(a,1);
-    agent_position_history(1,2,a)=agent_positions(a,2);
+a=1;
+for dy=0:grid_width
+    for dx=0:grid_width-1
 
+     if a <= N_agents
+         
+        agent_positions(a,1)=x+dx;
+        agent_positions(a,2)=y+dy;
+
+        agent_position_history(1,1,a)=agent_positions(a,1);
+        agent_position_history(1,2,a)=agent_positions(a,2);
+     
+        a=a+1;
+        
+     end
+    
+    end
 end
 
 % simulation starts
@@ -103,6 +124,49 @@ end
 
 clf
 
+%-----Figure to create target & estimate
+figure(1)
+for region=1:N_regions
+    
+    hold on
+    rectangle('Position',[region_info(region,1)-region_info(region,3),region_info(region,2)-region_info(region,3),region_info(region,3)*2,region_info(region,3)*2],'Curvature',[1 1],...
+        'FaceColor',[region_info(region,4),0,0],'LineStyle','none') %redder for higher concentration pollution
+end
+xlim([0 width_arena]), ylim([0 width_arena])
+set(gcf,'color','w')
+axis off
+F = getframe(gca);
+[X, ~] = frame2im(F);
+X=imresize(X,[width_arena,width_arena]);
+bw_target = imbinarize(rgb2gray(X));
+
+clf
+
+figure(1)
+agent_patch_radius=10;
+
+for a=1:N_agents
+    if agent_active(a)==0
+        hold on
+        rectangle('Position',[agent_position_history(end,1,a)-agent_patch_radius,agent_position_history(end,2,a)-agent_patch_radius,agent_patch_radius*2,agent_patch_radius*2],'Curvature',[1 1],...
+         'FaceColor','k','LineStyle','none') %redder for higher concentration pollution     
+    end
+end
+xlim([0 width_arena]), ylim([0 width_arena])
+set(gcf,'color','w')
+axis off
+F = getframe(gca);
+[X, ~] = frame2im(F);
+X=imresize(X,[width_arena,width_arena]);
+bw_estimate = imbinarize(rgb2gray(X));
+
+clf
+
+subplot(1,2,1),imshow(bw_target),title('Target')
+subplot(1,2,2),imshow(bw_estimate),title('Estimate')
+
+%---- Figure to show agent movement over time
+figure(2)
 subplot(1,2,1)
 
 %plot pollution patches
@@ -131,7 +195,7 @@ end
 xlim([0 width_arena]), ylim([0 width_arena])
 box on 
 set(gcf,'color','w')
-title('Agent trajectories over time')
+title('Agent trajectories over time','FontSize',14)
 
 subplot(1,2,2)
 
@@ -141,4 +205,60 @@ xlabel('Time steps')
 ylabel('Number of agents active')
 ylim([0 N_agents])
 xlim([0 T_sim+1])
-title('Number of active agents over time')
+title('Number of active agents over time','FontSize',14)
+
+%------- Figure to assess system performance
+figure(3)
+subplot(2,2,1)
+for region=1:N_regions
+    
+    hold on
+    rectangle('Position',[region_info(region,1)-region_info(region,3),region_info(region,2)-region_info(region,3),region_info(region,3)*2,region_info(region,3)*2],'Curvature',[1 1],...
+        'FaceColor',[region_info(region,4),0,0],'LineStyle','none') %redder for higher concentration pollution
+end
+
+xlim([0 width_arena]), ylim([0 width_arena])
+box on 
+set(gcf,'color','w')
+title('Unknown target environment','FontSize',14)
+
+subplot(2,2,2)
+for a=1:N_agents
+hold on
+
+    if agent_active(a)==1
+        hold on
+        plot(agent_position_history(end,1,a),agent_position_history(end,2,a),'ok','MarkerFaceColor','g'); %plot green if no pollution detected and still active
+    else
+        hold on
+        plot(agent_position_history(end,1,a),agent_position_history(end,2,a),'ok','MarkerFaceColor','r'); %plot red if pollution detected and stopped
+    end
+
+end
+xlim([0 width_arena]), ylim([0 width_arena])
+box on 
+set(gcf,'color','w')
+title('Estimated environment (points)','FontSize',14)
+
+subplot(2,2,3)
+
+agent_patch_radius=10;
+
+for a=1:N_agents
+    if agent_active(a)==0
+        hold on
+        rectangle('Position',[agent_position_history(end,1,a)-agent_patch_radius,agent_position_history(end,2,a)-agent_patch_radius,agent_patch_radius*2,agent_patch_radius*2],'Curvature',[1 1],...
+         'FaceColor','k','LineStyle','none') %redder for higher concentration pollution     
+    end
+end
+xlim([0 width_arena]), ylim([0 width_arena])
+box on
+
+similarity=jaccard(bw_estimate,bw_target);
+title({'Estimated environment (patches)',strcat('Jaccard similarity =',num2str(similarity))},'FontSize',14)
+
+subplot(2,2,4)
+histogram(agent_thresholds,20) %20 bins
+title('Agent threshold distribution','FontSize',14)
+ylabel('Count')
+xlabel('Threshold')
